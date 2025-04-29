25334665f4d8782ca4037a5b4d1a3205720e4fd8
lakshmi1113 <lakshmi.sharavati@gmail.com>
2025-04-29 16:48:41 +0530

    javacw

---
+++ student.java
@@ -0,0 +1,49 @@
+// public class student {
+//      static String cllgname;
+//      int rollno;
+//      String name;
+     
+//      public student(String name){
+//         this.name = name;
+//         this.rollno = setrollno();
+//      }
+
+//      static int counter = 0;
+//      public static int setrollno(){
+//         counter ++;
+//         return counter;
+//      }
+
+//      public static void setcllg(String name){
+//          cllgname = name;
+
+//      }
+
+//      public  void getstudentinfo(){
+//          System.out.println("student name:" + this.name);
+//          System.out.println("Student rollno:" + this.rollno);
+//          System.out.println("college name:" + cllgname);
+//      }
+
+//      public static void main(String[] args){
+//         student.setcllg("XYZ");
+//         student s1 = new student("Lakshmi");
+//         student s2 = new student("abc");
+
+//         s1.getstudentinfo();
+//         s2.getstudentinfo();
+//      }
+// }
+
+public class student{
+   private static String str = "GeeksforGeeks";
+   static class nestedclass{
+      public void disp(){
+         System.out.println(str);
+      }
+    }
+   public static void main(String[] args){
+      student.nestedclass obj = new student.nestedclass();
+      obj.disp();
+   }
+}
